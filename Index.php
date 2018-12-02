b2165b4608d4525682668476fbb898ac57f412c3
Mihail Curmei <mcurmei@mail.ru>
2018-12-02 23:11:56 +0100

    Created an html page for testing purposes

---
+++ Index.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+require("Domain\Separator.php");
+
+//$separator = new \WordSeparator\Domain\Separator("data/words.txt");
+$wordsManager = new \WordSeparator\Domain\FileReader("data/words.txt");
+
+?>
+
+<html>
+<body>
+	<?php
+		//echo count($me);
+		echo $wordsManager->getAmountFor(1);
+		echo $wordsManager->getAmount();
+		$wordsManager->printAll(true, true);
+		//$words = $separator->separate("donaudampfschiffkapitän");
+		
+		/*foreach($words as $word) {
+			echo("<p>$word</p>");
+		}*/
+	?>
+</body>
+</html>
