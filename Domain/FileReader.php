d5cd99b56959c1d5b99a4604b0252f3e2445b572
Mihail Curmei <mcurmei@mail.ru>
2018-12-02 23:12:37 +0100

    Switched to 2 dimensional arrays to fix the undefined size bug

--- Domain/FileReader.php
+++ Domain/FileReader.php
@@ -10,13 +10,19 @@ class FileReader {
 			$tempWords = array();
 			while (!feof($file)) {
 				// replacement to avoid .txt artifacts
-				$tempWords[] = preg_replace("/[^A-Za-zÄÖÜäöüß]/", '', fgets($file));
+				$word = preg_replace("/[^A-Za-zÄÖÜäöüß]/", '', fgets($file));
+				$tempWords[strlen($word)][] = $word;
 			}
 			
+			// this sort is purely for visual clearance and has no functional value
+			ksort($tempWords);
+			
 			// this section is normally not required, but since umlauts aren't sorted alphabetically in PHP, I have to use it here
-			sort($tempWords);
-			for ($tempWords as $word) {
-				$this->wordArray[] = $word;
+			foreach ($tempWords as $size => $wordGroup) {
+				sort($wordGroup);
+				foreach ($wordGroup as $word) {
+					$this->wordArray[$size][] = $word;
+				}
 			}
 			
 		} else {
@@ -24,29 +30,49 @@ class FileReader {
 		}
 	}
 	
+	function getAmountFor($size) {
+		return isset($this->wordArray[$size]) ? count($this->wordArray[$size]) : null;
+	}
+	
 	function getAmount() {
-		return count($this->wordArray);
+		$sum = 0;
+		foreach ($this->wordArray as $size => $wordGroup) {
+			$sum += $this->getAmountFor($size);
+		}
+		return $sum;
 	}
 
 	function getAll() {
 		return $this->wordArray;
 	}
 	
-	function getAt($index) {
-		return $this->wordArray[$index];
+	function getAt($size, $index) {
+		return $this->wordArray[$size][$index];
+	}
+	
+	function getFor($size) {
+		return $this->wordArray[$size];
 	}
 
 	// for testing purposes only
 	function printAll($indexed = false, $html = false) {
 		if ($html) { echo("<ul>"); }
-
-		for ($i = 0; $i < count($this->wordArray); $i++) {
+		
+		foreach ($this->wordArray as $size => $wordGroup) {
+			
 			if ($html) { echo("<li>"); }
-			echo(
-				($indexed ? "$i: " : "")
-				. $this->wordArray[$i]
-			);
-			if ($html) { echo("</li>"); }
+			if ($indexed) { echo("$size letters:"); }
+			if ($html) { echo("<ul>"); }
+			
+			foreach ($wordGroup as $index => $word) {
+				if ($html) { echo("<li>"); }
+				echo(
+					($indexed ? "$index: " : "")
+					. $word
+				);
+				if ($html) { echo("</li>"); }
+			}
+			if ($html) { echo("</ul></li>"); }
 		}
 
 		if ($html) { echo("</ul>"); }
