b1699e3f3eb38bc6658396d71861a3ce4fe74fe1
Mihail Curmei <mcurmei@mail.ru>
2018-12-03 01:58:12 +0100

    Renamed files to allow default access

--- Index.php
+++ Index.php
@@ -8,7 +8,7 @@ $separator = new \WordSeparator\Domain\Separator("data/words.txt");
 
 <html>
 <body>
-	<form action="Index.php" method="post">
+	<form action="index.php" method="post">
 		<input type="text" name="composite" style="width: 250px;" placeholder="Insert the composite word in lowercase" <?php if (isset($_POST['composite'])) { echo('value="'.$_POST['composite'].'"'); } ?> />
 		<input type="submit" name="submit" value="submit" />
 	</form>
