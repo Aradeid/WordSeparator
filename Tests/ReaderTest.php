9423cbe49655d27df6312b04973411451c9b4776
Mihail Curmei <mcurmei@mail.ru>
2018-12-03 02:19:12 +0100

    Added base for tests; Not finished; Not to be used in current version

---
+++ Tests/ReaderTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+//require("..\Domain\FileReader.php");
+
+final class EmailTest extends TestCase {
+	public function BadDictionaryTest() {
+		
+	}
+	
+	public function DictionarySizeTest() {
+		
+	}
+}
+
+?>
